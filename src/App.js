024083d50fb797519ebca47c9fb4a157375c8969
bar-amit <bar.amit@outlook.com>
2022-02-22 01:07:04 +0200

    feat: game module with basic ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Game from "./utils/2048";
+import "./App.css";
+
+var game = new Game();
 
 function App() {
+  const [board, setBoard] = useState(game.board);
+  const [status, setStatus] = useState(game.gameStatus);
+
+  function play(e) {
+    switch (e.key) {
+      case "ArrowLeft":
+      case "a":
+      case "A":
+        game.play("left");
+        break;
+      case "ArrowRight":
+      case "d":
+      case "D":
+        game.play("right");
+        break;
+      case "ArrowUp":
+      case "w":
+      case "W":
+        game.play("up");
+        break;
+      case "ArrowDown":
+      case "s":
+      case "S":
+        game.play("down");
+        break;
+    }
+    setBoard(game.board);
+    setStatus(game.gameStatus);
+  }
+
+  function addKeyboardEvent() {
+    document.addEventListener("keydown", play);
+  }
+
+  function removeKeyboardEvent() {
+    document.removeEventListener("keydown", play);
+  }
+
+  function startGame() {
+    game.initialize();
+    setBoard(game.board);
+    setStatus(game.gameStatus);
+    addKeyboardEvent();
+  }
+
+  useEffect(() => {
+    if (status !== "playing") removeKeyboardEvent();
+  }, [status]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>2048</h1>
       </header>
+      {status === null && (
+        <button className="button" type="button" onClick={startGame}>
+          start
+        </button>
+      )}
+      <div className="board">
+        {board.map((tile) => (
+          <div className="tile">{tile}</div>
+        ))}
+      </div>
+      <footer>Created by Bar Amit</footer>
     </div>
   );
 }
