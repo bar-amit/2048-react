ed370fc59712e18ccffeb67b5de7de74baa312ec
bar-amit <bar.amit@outlook.com>
2022-02-22 14:11:37 +0200

    fix: refactor to ES6 no bugs

--- src/App.js
+++ src/App.js
@@ -1,58 +1,66 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Game from "./utils/2048";
+import Board from "./components/Board";
+import Tile from "./components/Tile";
 import "./App.css";
 
 var game = new Game();
 
 function App() {
   const [board, setBoard] = useState(game.board);
-  const [status, setStatus] = useState(game.gameStatus);
+  const [status, setStatus] = useState(null);
 
-  function play(e) {
+  function init() {
+    setBoard(game.initialize());
+  }
+
+  function play(move) {
+    setBoard(game.play(move));
+  }
+
+  function playMove(e) {
+    removeKeyboardEvent();
     switch (e.key) {
       case "ArrowLeft":
       case "a":
       case "A":
-        game.play("left");
+        play("left");
         break;
       case "ArrowRight":
       case "d":
       case "D":
-        game.play("right");
+        play("right");
         break;
       case "ArrowUp":
       case "w":
       case "W":
-        game.play("up");
+        play("up");
         break;
       case "ArrowDown":
       case "s":
       case "S":
-        game.play("down");
+        play("down");
         break;
+      default:
+        addKeyboardEvent();
+        return;
     }
-    setBoard(game.board);
-    setStatus(game.gameStatus);
-  }
-
-  function addKeyboardEvent() {
-    document.addEventListener("keydown", play);
-  }
-
-  function removeKeyboardEvent() {
-    document.removeEventListener("keydown", play);
-  }
-
-  function startGame() {
-    game.initialize();
-    setBoard(game.board);
-    setStatus(game.gameStatus);
     addKeyboardEvent();
   }
 
-  useEffect(() => {
-    if (status !== "playing") removeKeyboardEvent();
-  }, [status]);
+  function addKeyboardEvent() {
+    document.addEventListener("keydown", playMove);
+  }
+
+  function removeKeyboardEvent() {
+    document.removeEventListener("keydown", playMove);
+  }
+
+  function startGame() {
+    init();
+    setStatus("playing");
+    addKeyboardEvent();
+  }
 
   return (
     <div className="App">
@@ -64,11 +72,11 @@ function App() {
           start
         </button>
       )}
-      <div className="board">
-        {board.map((tile) => (
-          <div className="tile">{tile}</div>
+      <Board>
+        {board.flat().map((tile, idx) => (
+          <Tile value={tile} key={`tile-${idx}`} />
         ))}
-      </div>
+      </Board>
       <footer>Created by Bar Amit</footer>
     </div>
   );
--- src/utils/2048.js
+++ src/utils/2048.js
@@ -1,121 +1,143 @@
-function TwoThousandFourtyEight() {
-  this.board = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-  ];
-  this.gameStatus = null;
+class TwoThousandFourtyEight {
+  constructor() {
+    this.board = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    this.gameStatus = null;
+  }
+  printBoard() {
+    this.board.forEach((row) => console.log(row));
+  }
+  initialize() {
+    this.gameStatus = "playing";
+    var [tile1, tile2] = [
+      {
+        index: getRandomIndex(),
+        value: getRandomTileValue(),
+      },
+      {
+        index: getRandomIndex(),
+        value: getRandomTileValue(),
+      },
+    ];
+    while (tile1.index == tile2.index) tile2.index = getRandomIndex();
+    this.board = setTile(this.board, tile1.index, tile1.value);
+    this.board = setTile(this.board, tile2.index, tile2.value);
+
+    return this.board;
+  }
+  play(direction) {
+    if (this.gameStatus != "playing") return this.board;
+
+    switch (direction) {
+      case "left":
+        this.moveLeft();
+        break;
+      case "right":
+        this.moveRight();
+        break;
+      case "up":
+        this.moveUp();
+        break;
+      case "down":
+        this.moveDown();
+        break;
+      default:
+        return this.board;
+    }
+    this.addNewTile();
+    this.updateGameStatus();
+
+    return this.board;
+  }
+  moveLeft() {
+    this.board = this.board.map(squeezLeft);
+  }
+  moveRight() {
+    this.board = this.board.map((row) => squeezLeft(row.reverse()).reverse());
+  }
+  moveUp() {
+    var newBoard = Array.from(this.board);
+    for (let i = 0; i < 4; i++) {
+      let column = newBoard.map((row) => row[i]);
+      column = squeezLeft(column);
+      column.forEach(
+        (tile, idx) => (newBoard = setTile(newBoard, idx * 4 + i, tile))
+      );
+    }
+    this.board = newBoard;
+  }
+
+  moveDown() {
+    var newBoard = Array.from(this.board);
+    for (let i = 0; i < 4; i++) {
+      let column = newBoard.map((row) => row[i]);
+      column = squeezLeft(column.reverse()).reverse();
+      column.forEach(
+        (tile, idx) => (newBoard = setTile(newBoard, idx * 4 + i, tile))
+      );
+    }
+    this.board = newBoard;
+  }
+
+  addNewTile() {
+    var emptyTiles = this.findEmptyTiles();
+    var tileIndex = getRandomIndex(emptyTiles.length - 1);
+    var tileValue = getRandomTileValue();
+
+    this.board = setTile(this.board, emptyTiles[tileIndex], tileValue);
+  }
+  findEmptyTiles() {
+    var res = [];
+
+    for (let i = 0; i < 4; i++)
+      for (let j = 0; j < 4; j++)
+        if (this.board[i][j] == 0) res.push(i * 4 + j);
+    return res;
+  }
+
+  getLegalMoves() {
+    let isLeftMoveLegal = this.board.some((row) => row[0] == 0);
+    let isRightMoveLegal = this.board.some((row) => row[3] == 0);
+    let isUpMoveLegal = this.board[0].some((tile) => tile == 0);
+    let isDownMoveLegal = this.board[3].some((tile) => tile == 0);
+
+    let isHorizontalMergeable = this.board.some(isMergeable);
+    let isVerticalMergeable = [0, 1, 2, 3].some((idx) =>
+      isMergeable([
+        this.board[0][idx],
+        this.board[1][idx],
+        this.board[2][idx],
+        this.board[3][idx],
+      ])
+    );
+
+    return {
+      left: isLeftMoveLegal || isHorizontalMergeable,
+      right: isRightMoveLegal || isHorizontalMergeable,
+      up: isUpMoveLegal || isVerticalMergeable,
+      down: isDownMoveLegal || isVerticalMergeable,
+    };
+  }
+  updateGameStatus() {
+    const { left, right, up, down } = this.getLegalMoves();
+
+    if (this.board.flat().some((tile) => tile == 2048)) this.gameStatus = "won";
+    else if (!left && !right && !up && !down) this.gameStatus = "lost";
+  }
 }
-TwoThousandFourtyEight.prototype.printBoard = function () {
-  this.board.forEach((row) => console.log(row));
-};
-TwoThousandFourtyEight.prototype.initialize = function () {
-  this.gameStatus = "playing";
-  var [tile1, tile2] = [
-    {
-      index: getRandomIndex(),
-      value: getRandomTileValue(),
-    },
-    {
-      index: getRandomIndex(),
-      value: getRandomTileValue(),
-    },
-  ];
-  while (tile1.index == tile2.index) tile2.index = getRandomIndex();
-  this.setTile(tile1.index, tile1.value);
-  this.setTile(tile2.index, tile2.value);
-};
-TwoThousandFourtyEight.prototype.play = function (direction) {
-  if (this.gameStatus != "playing") return;
-  switch (direction) {
-    case "left":
-      this.moveLeft();
-      break;
-    case "right":
-      this.moveRight();
-      break;
-    case "up":
-      this.moveUp();
-      break;
-    case "down":
-      this.moveDown();
-      break;
-    default:
-      return;
-  }
-  this.addNewTile();
-  this.updateGameStatus();
-};
-TwoThousandFourtyEight.prototype.moveLeft = function () {
-  this.board = this.board.map(squeezLeft);
-};
-TwoThousandFourtyEight.prototype.moveRight = function () {
-  this.board = this.board.map((row) => squeezLeft(row.reverse()).reverse());
-};
-TwoThousandFourtyEight.prototype.moveUp = function () {
-  for (let i = 0; i < 4; i++) {
-    let column = this.board.map((row) => row[i]);
-    column = squeezLeft(column);
-    column.forEach((tile, idx) => this.setTile(idx * 4 + i, tile));
-  }
-};
 
-TwoThousandFourtyEight.prototype.moveDown = function () {
-  for (let i = 0; i < 4; i++) {
-    let column = this.board.map((row) => row[i]);
-    column = squeezLeft(column.reverse()).reverse();
-    column.forEach((tile, idx) => this.setTile(idx * 4 + i, tile));
-  }
-};
-TwoThousandFourtyEight.prototype.addNewTile = function () {
-  var emptyTiles = this.findEmptyTiles();
-  var tileIndex = getRandomIndex(emptyTiles.length - 1);
-  var tileValue = getRandomTileValue();
-
-  this.setTile(emptyTiles[tileIndex], tileValue);
-};
-TwoThousandFourtyEight.prototype.findEmptyTiles = function () {
-  var res = [];
-
-  for (let i = 0; i < 4; i++)
-    for (let j = 0; j < 4; j++) if (this.board[i][j] == 0) res.push(i * 4 + j);
-  return res;
-};
-TwoThousandFourtyEight.prototype.setTile = function (tileNumber, value) {
+function setTile(board, tileNumber, value) {
+  var newArray = Array.from(board);
   var row = Math.floor(tileNumber / 4);
   var column = tileNumber % 4;
-  this.board[row][column] = value;
-};
-TwoThousandFourtyEight.prototype.getLegalMoves = function () {
-  let isLeftMoveLegal = this.board.some((row) => row[0] == 0);
-  let isRightMoveLegal = this.board.some((row) => row[3] == 0);
-  let isUpMoveLegal = this.board[0].some((tile) => tile == 0);
-  let isDownMoveLegal = this.board[3].some((tile) => tile == 0);
+  newArray[row][column] = value;
+  return newArray;
+}
 
-  let isHorizontalMergeable = this.board.some(isMergeable);
-  let isVerticalMergeable = [0, 1, 2, 3].some((idx) =>
-    isMergeable([
-      this.board[0][idx],
-      this.board[1][idx],
-      this.board[2][idx],
-      this.board[3][idx],
-    ])
-  );
-
-  return {
-    left: isLeftMoveLegal || isHorizontalMergeable,
-    right: isRightMoveLegal || isHorizontalMergeable,
-    up: isUpMoveLegal || isVerticalMergeable,
-    down: isDownMoveLegal || isVerticalMergeable,
-  };
-};
-TwoThousandFourtyEight.prototype.updateGameStatus = function () {
-  const { left, right, up, down } = this.getLegalMoves();
-
-  if (this.board.flat().some((tile) => tile == 2048)) this.gameStatus = "won";
-  else if (!left && !right && !up && !down) this.gameStatus = "lost";
-};
 function isMergeable(arr) {
   return arr[0] == arr[1] || arr[1] == arr[2] || arr[2] == arr[3];
 }
